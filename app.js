e93927a2b61e5f79d007b73b174d2aa57181edc3
Rory Hennessey <rory.hennessey@gmail.com>
2020-11-21 10:35:02 -0500

    show pizza email sign up button on scroll

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
-// Hide banner on scroll
+// Hide banner + Open Pizza btn on scroll
 window.addEventListener('scroll', function () {
 	const scrollPos = window.scrollY
 
 	const hideMe = document.getElementById('banner-element')
 
+	const showFob = document.getElementById('email-button')
+
 	if (scrollPos > 200) {
 		hideMe.classList.add('byeByeBanner')
+		showFob.classList.remove('email-button-hidden')
+		showFob.classList.add('email-button-show')
 	} else {
 		hideMe.classList.remove('byeByeBanner')
+		showFob.classList.remove('email-button-show')
+		showFob.classList.add('email-button-hidden')
 	}
 })
