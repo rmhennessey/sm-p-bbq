415105cf6ca55693221fa4859d06e246c0918b9b
Rory Hennessey <rory.hennessey@gmail.com>
2020-11-23 16:29:50 -0500

    adding more pizza build functionality

--- app.js
+++ app.js
@@ -31,20 +31,98 @@ function clearToppings() {
 	document.getElementById('bigToppings').style.backgroundImage = 'none'
 }
 
-function addPepperoni() {
-	// document.getElementById('bigToppings').style.backgroundImage =
-	// 	"url('./assets/byo_pepperoni.png')"
-	ingredients.push(`url('./assets/byo_pepperoni.png')`)
+function addAnchovies() {
+	ingredients.push(`url('./assets/byo_anchovies.png')`)
+	console.log(ingredients)
+	document.getElementById('bigToppings').style.backgroundImage = ingredients
+}
+
+function addBacon() {
+	ingredients.push(`url('./assets/byo_bacon.png')`)
+	console.log(ingredients)
+	document.getElementById('bigToppings').style.backgroundImage = ingredients
+}
+
+function addBroccoli() {
+	ingredients.push(`url('./assets/byo_broccoli.png')`)
 	console.log(ingredients)
 	document.getElementById('bigToppings').style.backgroundImage = ingredients
 }
 
 function addCheese() {
-	// document.getElementById('bigToppings').style.backgroundImage =
-	// 	"url('./assets/byo_cheese.png'), url('./assets/byo_pepperoni.png')"
 	ingredients.push(`url('./assets/byo_cheese.png')`)
 	console.log(ingredients)
 	document.getElementById('bigToppings').style.backgroundImage = ingredients
 }
 
-// document.getElementById("myDiv").style.backgroundImage = "url('img_tree.png')";
+function addCherryPeppers() {
+	ingredients.push(`url('./assets/byo_cherry_peppers.png')`)
+	console.log(ingredients)
+	document.getElementById('bigToppings').style.backgroundImage = ingredients
+}
+
+function addChicken() {
+	ingredients.push(`url('./assets/byo_chicken.png')`)
+	console.log(ingredients)
+	document.getElementById('bigToppings').style.backgroundImage = ingredients
+}
+
+function addGarlic() {
+	ingredients.push(`url('./assets/byo_garlic.png')`)
+	console.log(ingredients)
+	document.getElementById('bigToppings').style.backgroundImage = ingredients
+}
+
+function addMushrooms() {
+	ingredients.push(`url('./assets/byo_mushrooms.png')`)
+	console.log(ingredients)
+	document.getElementById('bigToppings').style.backgroundImage = ingredients
+}
+
+function addOnions() {
+	ingredients.push(`url('./assets/byo_onions.png')`)
+	console.log(ingredients)
+	document.getElementById('bigToppings').style.backgroundImage = ingredients
+}
+
+function addPepperoni() {
+	ingredients.push(`url('./assets/byo_pepperoni.png')`)
+	console.log(ingredients)
+	document.getElementById('bigToppings').style.backgroundImage = ingredients
+}
+
+function addPeppers() {
+	ingredients.push(`url('./assets/byo_peppers.png')`)
+	console.log(ingredients)
+	document.getElementById('bigToppings').style.backgroundImage = ingredients
+}
+
+function addProsciutto() {
+	ingredients.push(`url('./assets/byo_prosciutto.png')`)
+	console.log(ingredients)
+	document.getElementById('bigToppings').style.backgroundImage = ingredients
+}
+
+function addSausage() {
+	ingredients.push(`url('./assets/byo_sausage.png')`)
+	console.log(ingredients)
+	document.getElementById('bigToppings').style.backgroundImage = ingredients
+}
+
+function addShrimp() {
+	ingredients.push(`url('./assets/byo_shrimp.png')`)
+	console.log(ingredients)
+	document.getElementById('bigToppings').style.backgroundImage = ingredients
+}
+
+function addSoppresatta() {
+	ingredients.push(`url('./assets/byo_soppresatta.png')`)
+	console.log(ingredients)
+	document.getElementById('bigToppings').style.backgroundImage = ingredients
+}
+
+function addSpinach() {
+	ingredients.push(`url('./assets/byo_spinach.png')`)
+	console.log(ingredients)
+	document.getElementById('bigToppings').style.backgroundImage = ingredients
+}
